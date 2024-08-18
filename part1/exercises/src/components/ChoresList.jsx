78cf94a6560cfb247dce923a9f034b981300d73d
mlasch01 <mark.lasch@yahoo.com>
2024-08-18 13:24:45 -0500

    update1

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,44 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+// export default function ChoresList () {
+//    return <p>No content is here!</p>;
+// }
+
+
+
+
+import React from 'react';
+import classes from './ChoresList.module.css';
+
+class ChoresList extends React.Component {
+   render() {
+      const chores = ["Take out trash", "Wash the dishes"];
+      return (  
+      <div>
+         <h3 className = {classes.choresHeading}>Chores to Finish</h3>
+         <ol>
+            <li className={classes.choresText}>{chores[0]}</li>
+            <li className={classes.choresText}>{chores[1]}</li>
+         </ol>
+      </div>
+      );
+   }
 }
+
+export default ChoresList;
+
+
+
+//    render() {
+//       const movies = ["Pride and Prejudice", "Howl's Moving Castle"];
+//       return (
+//       <div>
+//          <h3 className = {classes.movieHeading}>Movies I Watched This Month</h3>
+//          <ol>
+//             <li className={classes.movieText}>{movies[0]}</li>
+//             <li className={classes.movieText}>{movies[1]}</li>
+//          </ol>
+//       </div>
+//       );
+//    }
+// }
+
+// export default MovieList;
