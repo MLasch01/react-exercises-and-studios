38799c0f3d0d24c9bcbb448c53079683bef45218
mlasch01 <mark.lasch@yahoo.com>
2024-08-22 16:16:54 -0500

    complated

--- part2/exercises-2/src/components/Button.jsx
+++ part2/exercises-2/src/components/Button.jsx
@@ -1,9 +1,12 @@
 import './styles.css';
 
 function Button() {
+   function onLearnMore() {
+      return alert("Slipsh Splash");
+   }
 
    return ( 
-         <button>
+         <button onClick={() =>onLearnMore()}>
             Learn More
          </button>
    );
--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,15 +1,55 @@
 import './styles.css';
 import Button from './Button';
-
+import oceans from './oceans.json';
 
 function Profile()
 {
    return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
+      <div>
+         <Fish />
+         <FishName />
+         <Facts />
+         <ul>
+            {listItem}
+            
+         </ul>
          <Button />
-      </>  
+      </div>
+         
    );
 }
+
+const listItem = oceans.map(oceans =>
+   <div key={oceans.id} className="profile" >
+      <img src={oceans.image} alt={oceans.name} className="img"/>
+   </div>
+);
+
+function Fish() {
+   return(
+   <div key={oceans.id}  className={`${oceans.fishCheck === "true" ? "isAFish" : "profile"}`}>
+      </div>
+   );
+}
+
+function FishName() {
+   return(
+      <h1>{oceans.name}</h1>
+   );
+}
+
+function Facts(){
+   return(
+      <>
+         <h3>Fun Facts:</h3>
+         <ol>
+            <li>{oceans.fact1}</li>
+            <li>{oceans.fact2}</li>
+            <li>{oceans.fact3}</li>
+         </ol>
+      </>
+   );
+}
+
 
 export default Profile;
